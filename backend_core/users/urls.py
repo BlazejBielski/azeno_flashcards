ae246fed917db43adf08293a0535093cb091f72b
BlazejBielski <Blazej Bielski>
2023-02-09 23:07:02 +0100

    added token jwt, user create, user logout, user custom manager

--- backend_core/users/urls.py
+++ backend_core/users/urls.py
@@ -5,12 +5,18 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from users import views
+
+
 app_name = 'users'
 
 urlpatterns = [
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.CustomUserCreateView.as_view(), name='user_register'),
+    path('logout/', views.BlackListTokenUpdateView.as_view(), name='user_logout'),
+
 
 ]
 
